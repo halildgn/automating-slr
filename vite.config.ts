380fbd49fbcddbefeb5328a8f3002f349a3a8038
Halil Dogan <halildogan1996@gmail.com>
2024-03-29 15:19:44 +0100

    adjust vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,18 +5,9 @@ import { viteSingleFile } from "vite-plugin-singlefile"
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [react(), viteSingleFile()],
-
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
   clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
   server: {
     port: 1420,
     strictPort: true,
-    watch: {
-      // 3. tell vite to ignore watching `src-tauri`
-      ignored: ["**/src-tauri/**"],
-    },
   },
 }));
