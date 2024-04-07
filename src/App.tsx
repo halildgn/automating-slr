3f0687e5843bfd82f4d146e3d88a44cec9594d78
Halil Dogan <halildogan1996@gmail.com>
2024-04-07 19:37:03 +0200

    add theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import Tabs from "@mui/material/Tabs";
+import IconButton from '@mui/material/IconButton';
 import Tab from "@mui/material/Tab";
 import Box from "@mui/material/Box";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import QueryGenerator from "./components/QueryGenerator";
 import Filtering from "./components/Filtering";
 import { COMPONENTS } from "./types/index";
@@ -15,13 +18,39 @@ import Download from "./components/Download";
 import colapsLogo from './assets/colaps.png';
 
 function App() {
-  const darkTheme = createTheme({
+  // let theme: 'light' | 'dark'
+  // useEffect(() => {
+  //   getCurrentTheme();
+  // }, []);
+
+  // function getCurrentTheme(){
+  //   return localStorage.getItem('theme') ??
+  // }
+
+  // function setTheme(){
+
+  // }
+
+  const [isDark, setToDark] = useState<boolean>(false); 
+
+
+  
+  const dark = createTheme({
     palette: {
       mode: "dark",
     },
   });
+  const light = createTheme({
+    palette: {
+      mode: "light",
+    },
+  });
 
   const [component, setComponent] = useState<COMPONENTS>(0);
+
+  function toggleTheme(){
+    setToDark(!isDark);
+  }
 
   function samePageLinkNavigation(
     event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
@@ -100,11 +129,16 @@ function App() {
 
   return (
     <>
-      <ThemeProvider theme={darkTheme}>
+      <ThemeProvider theme={ isDark ? dark : light}>
         <CssBaseline />
         <div className={component === COMPONENTS.DOWNLOAD ? 'download-container' : 'container'}>
           <Box sx={{ width: "100%" }}>
     <img src={colapsLogo} className="colaps-logo"/>
+            <div style={{position: 'absolute', top: '3%', right: '5%', zIndex: 1000}}>
+   <IconButton onClick={toggleTheme} color="inherit">
+        {isDark ? <Brightness7Icon /> : <Brightness4Icon />}
+      </IconButton>
+      </div>
             <Tabs
               value={component}
               onChange={handleChange}
